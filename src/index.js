7332d7395276e696bfa5f095f3f1a1a78db82d68
Yujin Wang <ywang2589@wisc.edu>
2023-10-14 18:18:22 -0500

    update render object

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={studioTheme}>
-    <App />
+    {/* <App /> */}
     <Web1920 />
     </ThemeProvider>
   </React.StrictMode>
